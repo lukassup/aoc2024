f307136c597540eac29a1842a20ece62c0705ded
Lukas Supienis <lukas.supienis@vinted.com>
2024-12-11 01:47:32 +0200

    day10p2

--- src/day10.rs
+++ src/day10.rs
@@ -13,20 +13,19 @@ const NEIGH_OFFSET: [(isize, isize); 4] = [
     (-1, 0), // left
 ];
 
-fn iter_neigh(grid: &[Vec<Elevation>], point: Point) -> impl Iterator<Item = Point> {
-    let x_o = point.0;
-    let y_o = point.1;
+fn iter_neigh(grid: &[Vec<Elevation>], start: Point) -> impl Iterator<Item = Point> {
+    let (x_start, y_start) = start;
     let x_max = grid[0].len();
     let y_max = grid.len();
-    let start_el = &grid[y_o][x_o];
+    let val_start = &grid[y_start][x_start];
     NEIGH_OFFSET.iter().filter_map(move |&(dx, dy)| {
-        let x = x_o.checked_add_signed(dx)?;
-        let y = y_o.checked_add_signed(dy)?;
+        let x = x_start.checked_add_signed(dx)?;
+        let y = y_start.checked_add_signed(dy)?;
         if x >= x_max || y >= y_max {
             return None;
         }
-        let neigh_el = grid.get(y).and_then(|row| row.get(x))?;
-        if neigh_el - *start_el != 1 {
+        let val_neigh = grid.get(y).and_then(|row| row.get(x))?;
+        if val_neigh - *val_start != 1 {
             return None;
         }
         Some((x, y))
@@ -35,15 +34,31 @@ fn iter_neigh(grid: &[Vec<Elevation>], point: Point) -> impl Iterator<Item = Poi
 
 fn bfs(map: &[Vec<Elevation>], start: Point) -> HashSet<Point> {
     let mut queue: VecDeque<Point> = VecDeque::new();
-    let mut visited: HashSet<Point> = HashSet::new();
+    let mut visited: HashSet<Point> = HashSet::new(); // !
     queue.push_back(start);
-    while let Some(pos) = queue.pop_front()  {
-        visited.insert(pos);
-        for (x,y) in iter_neigh(map, pos) {
-            if visited.contains(&(x,y)) {
+    while let Some(point) = queue.pop_front()  {
+        visited.insert(point);
+        for neigh_point in iter_neigh(map, point) {
+            if visited.contains(&neigh_point) {
                 continue;
             }
-            queue.push_back((x,y));
+            queue.push_back(neigh_point);
+        }
+    }
+    visited
+}
+
+fn bfs_vec(map: &[Vec<Elevation>], start: Point) -> Vec<Point> {
+    let mut queue: VecDeque<Point> = VecDeque::new();
+    let mut visited: Vec<Point> = Vec::new(); // !
+    queue.push_back(start);
+    while let Some(point) = queue.pop_front()  {
+        visited.push(point);
+        for neigh_point in iter_neigh(map, point) {
+            if visited.contains(&neigh_point) {
+                continue;
+            }
+            queue.push_back(neigh_point);
         }
     }
     visited
@@ -77,15 +92,30 @@ pub fn part1<T: AsRef<Path>>(path: T) -> io::Result<usize> {
     Ok(result)
 }
 
-pub fn part2<T: AsRef<Path>>(path: T) -> io::Result<u64> {
+pub fn part2<T: AsRef<Path>>(path: T) -> io::Result<usize> {
     let file = File::open(path)?;
     let buf = BufReader::new(file);
-    let _map: Vec<Vec<u32>> = buf
+
+    let map: Vec<Vec<Elevation>> = buf
         .lines()
         .map_while(Result::ok)
         .map(|s| s.chars().flat_map(|c| c.to_digit(10)).collect())
         .collect();
-    todo!();
-    let result: u64 = 0;
+
+    let mut trailheads: Vec<Point> = Vec::new();
+    for (y, line) in map.iter().enumerate() {
+        for (x, &elevation) in line.iter().enumerate() {
+            if elevation == 0 {
+                trailheads.push((x, y));
+            }
+        }
+    }
+
+    let mut result: usize = 0;
+    for start in trailheads {
+        let visited = bfs_vec(&map, start);
+        let score = visited.iter().filter(|&&(x, y)| map[y][x] == 9).count();
+        result += score;
+    }
     Ok(result)
 }
