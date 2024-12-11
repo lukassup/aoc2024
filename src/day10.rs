f0aeae88a215ed06e435d9c52eb732ea1e3d08b3
Lukas Supienis <lukas.supienis@vinted.com>
2024-12-11 20:33:09 +0200

    day10 fix panic in debug

--- src/day10.rs
+++ src/day10.rs
@@ -8,8 +8,8 @@ type Elevation = u32;
 
 const NEIGH_OFFSET: [(isize, isize); 4] = [
     (0, -1), // up
-    (1, 0), // right
-    (0, 1), // down
+    (1, 0),  // right
+    (0, 1),  // down
     (-1, 0), // left
 ];
 
@@ -25,7 +25,7 @@ fn iter_neigh(grid: &[Vec<Elevation>], start: Point) -> impl Iterator<Item = Poi
             return None;
         }
         let val_neigh = grid.get(y).and_then(|row| row.get(x))?;
-        if val_neigh - *val_start != 1 {
+        if val_neigh.saturating_sub(*val_start) != 1 {
             return None;
         }
         Some((x, y))
@@ -36,7 +36,7 @@ fn bfs(map: &[Vec<Elevation>], start: Point) -> HashSet<Point> {
     let mut queue: VecDeque<Point> = VecDeque::new();
     let mut visited: HashSet<Point> = HashSet::new(); // !
     queue.push_back(start);
-    while let Some(point) = queue.pop_front()  {
+    while let Some(point) = queue.pop_front() {
         visited.insert(point);
         for neigh_point in iter_neigh(map, point) {
             if visited.contains(&neigh_point) {
@@ -52,7 +52,7 @@ fn bfs_vec(map: &[Vec<Elevation>], start: Point) -> Vec<Point> {
     let mut queue: VecDeque<Point> = VecDeque::new();
     let mut visited: Vec<Point> = Vec::new(); // !
     queue.push_back(start);
-    while let Some(point) = queue.pop_front()  {
+    while let Some(point) = queue.pop_front() {
         visited.push(point);
         for neigh_point in iter_neigh(map, point) {
             if visited.contains(&neigh_point) {
